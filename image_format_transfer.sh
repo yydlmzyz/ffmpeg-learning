ae605fc6fe798fe26384f3ee857a990fb7d848a0
yydlmzyz <1139970854@qq.com>
2018-04-29 01:15:30 +0800

    add image format transfer

--- image_format_transfer.sh
+++ image_format_transfer.sh
@@ -1,1 +1,9 @@
-# ffmpeg-learning
+#!/bin/bash
+
+#transfer image format from jpg to png and change image size from 481*321 to 480*320
+cd ./images/481x321/
+for filename in *.jpg
+do
+      ffmpeg -f image2 -i $filename -f image2 -s 480*320 ../../images_png/480x320/${filename%.*}.png
+done
+ 				
